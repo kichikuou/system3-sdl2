72e1fa438c0350134324ff79b7d029d0a966e106
kichikuou <KichikuouChrome@gmail.com>
2019-03-31 16:16:16 +0900

    Fix for 64-bit systems

--- src/common.h
+++ src/common.h
@@ -26,12 +26,12 @@ typedef char _TCHAR;
 #define _stprintf_s sprintf_s
 
 // type definition
-typedef unsigned char uint8;
-typedef unsigned short uint16;
-typedef unsigned long uint32;
-typedef signed char int8;
-typedef signed short int16;
-typedef signed long int32;
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
 
 
 #ifndef WIN32
