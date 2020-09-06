4229ca69106c077a56533cb222a7502f67fe4462
kichikuou <KichikuouChrome@gmail.com>
2020-09-06 11:31:33 +0900

    emscripten: Introduce nact_emscripten.cpp
    And use tvar_maxlen in text_dialog().

---
+++ src/emscripten/nact_emscripten.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "nact.h"
+#include "utfsjis.h"
+#include <emscripten.h>
+
+void NACT::text_dialog()
+{
+	static char buf[256];
+	char *oldstr = sjis2utf(tvar[tvar_index - 1]);
+	int ok = EM_ASM_({
+			var r = xsystem35.shell.inputString("文字列を入力してください", UTF8ToString($0), $1);
+			if (r) {
+				stringToUTF8(r, $2, $3);
+				return 1;
+			}
+			return 0;
+		}, oldstr, tvar_maxlen, buf, sizeof buf);
+	free(oldstr);
+	if (ok) {
+		char *newstr = utf2sjis(buf);
+		strcpy_s(tvar[tvar_index - 1], 22, newstr);
+		free(newstr);
+	}
+}
+
+void NACT::initialize_console()
+{
+}
+
+void NACT::release_console()
+{
+}
+
+void NACT::output_console(const char *format, ...)
+{
+#if defined(_DEBUG_CONSOLE)
+	va_list ap;
+
+	va_start(ap, format);
+	vfprintf(stdout, format, ap);
+	va_end(ap);
+#endif
+}
