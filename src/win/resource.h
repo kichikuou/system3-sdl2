4244fe63f521cc8fa607c753888ad776d12b03de
kichikuou <KichikuouChrome@gmail.com>
2021-09-05 15:30:50 +0900

    Clean up win/resource.rc

--- src/win/resource.h
+++ src/win/resource.h
@@ -1,7 +1,6 @@
-//{{NO_DEPENDENCIES}}
-// Microsoft Developer Studio generated include file.
-// Used by Script1.rc
-//
+#ifndef _RESOURCE_H_
+#define _RESOURCE_H_
+
 #define IDR_MENU1                       101
 #define IDI_ICON1                       102
 #define IDD_DIALOG1                     103
@@ -31,13 +30,4 @@
 #define ID_TEXT_SKIP                    40008
 #define ID_TEXT_WAIT                    40009
 
-// Next default values for new objects
-// 
-#ifdef APSTUDIO_INVOKED
-#ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        118
-#define _APS_NEXT_COMMAND_VALUE         40009
-#define _APS_NEXT_CONTROL_VALUE         1000
-#define _APS_NEXT_SYMED_VALUE           101
-#endif
-#endif
+#endif // _RESOURCE_H_
