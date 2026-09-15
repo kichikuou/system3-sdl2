647a5aa32603270d2037d2cf3829189cae250067
kichikuou <KichikuouChrome@gmail.com>
2026-09-15 17:29:29 +0900

    Split music playback from decoding
    Move the playback stream into generic/mako.cpp and leave the file
    format decoders in music_decoder.cpp.

---
+++ src/sys/music_decoder.h
@@ -0,0 +1,24 @@
+#ifndef MUSIC_DECODER_H_
+#define MUSIC_DECODER_H_
+
+#include <memory>
+#include <string>
+#include <SDL.h>
+
+struct DecodedChunk {
+	const Uint8* data;
+	int frames;
+};
+
+class MusicDecoder {
+public:
+	virtual ~MusicDecoder() = default;
+	virtual bool is_open() const = 0;
+	virtual const SDL_AudioSpec& spec() const = 0;
+	virtual DecodedChunk decode(int max_frames) = 0;
+	virtual void seek_start() = 0;
+};
+
+std::unique_ptr<MusicDecoder> create_music_decoder(const std::string& path);
+
+#endif // MUSIC_DECODER_H_
